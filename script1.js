23fb3a857ce8e4a567fc370797aaf3d24c94ab93
bmit-vom-espas <melvin.volmar@espas.ch>
2024-10-10 09:17:24 +0200

    Aktualisiere Webseite mit neuen Änderungen

--- script1.js
+++ script1.js
@@ -5,3 +5,12 @@ function scrollToTop() {
     });
 }
 
+// Event Listener für den "Kein Interesse"-Button
+document.getElementById("KeinInteresse-btn").addEventListener("click", function(event) {
+    event.preventDefault(); // Verhindert das Standardverhalten des Buttons
+    window.location.href = 'keinteresse.html'; // Neue Seite öffnen
+});
+
+
+
+
