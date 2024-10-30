e750cc9c49d5710091bccf13386d63eae478947c
bi-it-vom <144432512+bi-it-vom@users.noreply.github.com>
2024-10-30 08:58:57 +0100

    Add files via upload

--- script1.js
+++ script1.js
@@ -5,11 +5,6 @@ function scrollToTop() {
     });
 }
 
-// Event Listener für den "Kein Interesse"-Button
-document.getElementById("KeinInteresse-btn").addEventListener("click", function(event) {
-    event.preventDefault(); // Verhindert das Standardverhalten des Buttons
-    window.location.href = 'keinteresse.html'; // Neue Seite öffnen
-});
 
 
 
